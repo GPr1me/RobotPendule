e1ecf8b3fbcda7a4e2ecfdff91a27685ff04ad40
nasassx <43653908+nasassx@users.noreply.github.com>
2019-07-30 19:36:46 -0400

    version sur portable et pi merge

--- IdentificationParams/identificationArduino/src/main.cpp
+++ IdentificationParams/identificationArduino/src/main.cpp
@@ -560,7 +560,7 @@ double computePIDAng(){
       return angle;
     }
   }
-  return getAngle()+getAngleSpeed()*0;
+  return getAngle();
 }
 
 void PIDcommand(double cmd){
@@ -604,6 +604,7 @@ void PIDgoalReached(){
   AX_.resetEncoder(0);
   //attends a que l'angle soit negatif
   while(getAngle() < 0){
+    //condition do exit if no wait needed
     if(countA > 3){
       break;
     }
