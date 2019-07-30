728092f78f71e24d04968ffe019b79c01fa53fe9
GPr1me <marco_guitar_master@hotmail.com>
2019-07-30 14:57:23 -0400

    fixed PID_angle callback error

--- IdentificationParams/identificationArduino/myLibraries/PID_angle.cpp
+++ IdentificationParams/identificationArduino/myLibraries/PID_angle.cpp
@@ -12,7 +12,7 @@ void PID_angle::run()
     if (enable_)
     {
         unsigned long initTW = millis();
-        if (getAngle() < goal_ && getAngle() > -goal_ && enable_)
+        if (angleCommand_() < goal_ && angleCommand_() > -goal_ && enable_)
         {
             unsigned long tWave = millis() - initTW;
             double scmd = 0.4 * sin(4.8 * tWave / 1000.0);
